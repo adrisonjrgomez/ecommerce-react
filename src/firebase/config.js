4e2112e013474e82261531cc9926dbde1bcb9a55
Adrison Gomez <adrison.gomez@hotmail.com>
2020-04-01 20:50:14 -0400

    [NEW] Add firebase API-

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,5 +1,9 @@
-const config = {
-    apiKey: "AIzaSyCy8eh_7Of6QoF1TQYMPkBw43Fna6PIk4o",
+
+const config = () => {
+  const apiKey = process.env;
+  console.log(apiKey);
+  return {
+    apiKey: apiKey,
     authDomain: "ecommerce-reactjs-fe17a.firebaseapp.com",
     databaseURL: "https://ecommerce-reactjs-fe17a.firebaseio.com",
     projectId: "ecommerce-reactjs-fe17a",
@@ -7,5 +11,6 @@ const config = {
     messagingSenderId: "203105989564",
     appId: "1:203105989564:web:02acf89c76628fe25431b9"
   };
+}
 
 export default config;
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,7 +3,9 @@ import "firebase/firestore";
 import "firebase/auth";
 import config from './config'
 
-firebase.initializeApp(config);
+console.log(config());
+firebase.initializeApp(config());
+
 
 export const auth = firebase.auth();
 
